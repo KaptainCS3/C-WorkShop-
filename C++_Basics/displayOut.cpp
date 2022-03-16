8b919a93632343f523411733c0cce5e76fd392ce
KaptainCS3 <mbiapplegryn@gmail.com>
2022-03-17 00:16:41 +0100

    display function c++

---
+++ C++_Basics/displayOut.cpp
@@ -0,0 +1,11 @@
+/*
+ *  Author: KaptainCS3
+ *  Description: display function
+ *  Date: 17th March 2022
+ */
+
+#include<iostream>
+using namespace std;
+int main(void){
+    cout << "hello C++";    //!keyword "cout <<" to echo message to the screen
+}
