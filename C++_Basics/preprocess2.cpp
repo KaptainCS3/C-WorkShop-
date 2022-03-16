ac6813096c63df70fcf68981a5ca7d92737c9a61
KaptainCS3 <mbiapplegryn@gmail.com>
2022-03-17 00:18:04 +0100

    define function from standard library to use in program

---
+++ C++_Basics/preprocess2.cpp
@@ -0,0 +1,14 @@
+/*
+ *  Author: KaptainCS3
+ *  Description: C++ structure
+ *  Date: 17th March 2022
+ */
+
+#include<iostream>
+//!Define function from standard library to use
+
+using std::cout;
+using std::endl;
+int main(void){
+    cout<< "Hello World" << endl;
+}
