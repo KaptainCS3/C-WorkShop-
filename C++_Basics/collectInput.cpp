e444a69343687c14867ddbdda40accef7aae5c6b
KaptainCS3 <mbiapplegryn@gmail.com>
2022-03-17 00:16:18 +0100

    collect input function c++

---
+++ C++_Basics/collectInput.cpp
@@ -0,0 +1,14 @@
+/*
+ *  Author: KaptainCS3
+ *  Description: collect input function
+ *  Date: 17th March 2022
+ */
+
+ #include<iostream>
+ using namespace std;
+ int main(void){
+     char str[20];
+     cout << " Enter a string" << endl;
+     cin >> str;    //! taking input from user "cin >>" standard library function
+     cout << str << endl;   //!echo output to screen 
+ }
