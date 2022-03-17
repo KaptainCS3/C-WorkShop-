43e31f33c815eb1c7bd35980737266c0be2e443a
KaptainCS3 <mbiapplegryn@gmail.com>
2022-03-17 19:01:43 +0100

    arithmetic operators

---
+++ C++_Basics/arithOperator.cpp
@@ -0,0 +1,24 @@
+/*
+ *  Author: KaptainCS3
+ *  Description: display function
+ *  Date: 17th March 2022
+ */
+
+ #include<iostream>
+ using namespace std;
+
+ int main(void){
+    int a = 10, b = 4;
+    int sum, sub, mul, div, mod;
+    sum = a + b;    //! addition
+    cout << sum << endl;
+    sub = a - b;    //! subtration
+    cout << sub << endl;
+    mul = b * sub;  //! multiplication
+    cout << mul << endl;
+    div = sum / mul;    //! division
+    cout << div << endl;
+    mod = a % b;    //! modulus
+    cout << mod << endl;
+
+ }
