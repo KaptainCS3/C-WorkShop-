0ed93315a42a8399fb560cdd44adf0aa1fec16c8
KaptainCS3 <mbiapplegryn@gmail.com>
2022-03-17 18:59:53 +0100

    variable cpp

---
+++ C++_Basics/variable.cpp
@@ -0,0 +1,18 @@
+/*
+ *  Author: KaptainCS3
+ *  Description: display function
+ *  Date: 17th March 2022
+ */
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int guessNum = 10;
+    cout << guessNum << endl;
+    int a = 30;
+    int b = 12;
+    int sum = a + b;
+    cout << "sum of a + b :" << sum;
+    return 0;
+}
