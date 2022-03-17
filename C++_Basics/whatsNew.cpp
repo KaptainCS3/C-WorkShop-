8eb0828d7b91fe6fdc0532212b28d7139812550d
KaptainCS3 <mbiapplegryn@gmail.com>
2022-03-17 19:02:22 +0100

    auto keyword detect data type variable

---
+++ C++_Basics/whatsNew.cpp
@@ -0,0 +1,16 @@
+/*
+ *  Author: KaptainCS3
+ *  Description: display function
+ *  Date: 17th March 2022
+ */
+
+#include<iostream>
+using namespace std;
+
+//! Any variable declared with the auto keyword should be initialized at the time of its declaration or the will be an error.
+int main(void){
+    auto x = 4; //! integer
+    auto y = 3.37; //!  float
+    auto z = "Hello"; //!   string
+    return 0;
+}
