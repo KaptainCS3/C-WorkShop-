48e8b44b77e479dc20da408caff136a192a6f0f7
KaptainCS3 <mbiapplegryn@gmail.com>
2022-03-19 20:26:48 +0100

    conditions

---
+++ Conditions and Loops/ifstatement.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(void){
+    int a, b, sum;
+    cin >> a;
+    cin >> b;
+
+    if(a > b){
+        cout << "surrender your self to the cops\n";
+        cout << "number of crimes is : " << a << endl;
+    }
+}
